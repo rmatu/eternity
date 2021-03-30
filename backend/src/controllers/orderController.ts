b25f90cd643ed67ea54bb2d6eb67d34111b09d23
Ryszard Matula <rmatula@us.edu.pl>
2021-03-30 20:29:00 +0200

    Backend | Order controller

---
+++ backend/src/controllers/orderController.ts
@@ -0,0 +1,63 @@
+import expressAsyncHandler from "express-async-handler";
+import { SHIPPING_PRICE } from "../constants";
+import Order from "../models/orderModel";
+import Product from "../models/productModel";
+import { mergeTwoArraysOfObject } from "../utils";
+
+export const postOrder = expressAsyncHandler(async (req, res) => {
+  const order = new Order({
+    orderItems: req.body.orderItems,
+    shippingAddress: req.body.shippingAddress,
+    paymentResult: req.body.paymentResult,
+    itemsPrice: req.body.itemsPrice,
+    shippingPrice: req.body.shippingPrice,
+    totalPrice: req.body.totalPrice,
+    paidAt: req.body.paidAt,
+    isDelivered: req.body.isDelivered,
+    deliveredAt: req.body.deliveredAt,
+    email: req.body.email,
+    user: req.body.user || null,
+  });
+
+  const createdProduct = await order.save();
+  res.send(createdProduct);
+});
+
+export const totalProductsPrice = expressAsyncHandler(async (req, res) => {
+  //@ts-ignore
+  const productsIds = req.body.productsList.map((el) => el.productId);
+  const productsList = await Product.find({
+    _id: { $in: productsIds },
+  });
+
+  if (!productsList) {
+    res.status(404).send("Product doesn't exist");
+    return;
+  }
+
+  const singleProductArray = productsList.map((el) => ({
+    id: el._id,
+    price: el.price,
+  }));
+
+  const mergedArray = mergeTwoArraysOfObject(singleProductArray, req.body.productsList);
+
+  const totalPrice = mergedArray.map((el) => el.qty * el.price).reduce((a, b) => a + b, 0);
+
+  res.send({ price: totalPrice + SHIPPING_PRICE });
+});
+
+export const getAllUserOrders = expressAsyncHandler(async (req, res) => {
+  //@ts-ignore
+  const skip = parseInt(req.query.skip);
+  //@ts-ignore
+  const limit = parseInt(req.query.limit);
+
+  const orders = await Order.find({ user: req.params.id }).sort({ _id: -1 }).skip(skip).limit(limit);
+
+  if (!orders) {
+    res.status(404).send({ message: "Orders not Found" });
+    return;
+  }
+  res.send(orders);
+});
--- backend/src/routers/orderRouter.ts
+++ backend/src/routers/orderRouter.ts
@@ -1,78 +1,12 @@
 import express from "express";
-import expressAsyncHandler from "express-async-handler";
-import { SHIPPING_PRICE } from "../constants";
-import Order from "../models/orderModel";
-import Product from "../models/productModel";
-import { isAuth, mergeTwoArraysOfObject } from "../utils";
+import * as orderController from "../controllers/orderController";
+import { isAuth } from "../utils";
 
 const orderRouter = express.Router();
 
-orderRouter.post(
-  "/",
-  expressAsyncHandler(async (req, res) => {
-    const order = new Order({
-      orderItems: req.body.orderItems,
-      shippingAddress: req.body.shippingAddress,
-      paymentResult: req.body.paymentResult,
-      itemsPrice: req.body.itemsPrice,
-      shippingPrice: req.body.shippingPrice,
-      totalPrice: req.body.totalPrice,
-      paidAt: req.body.paidAt,
-      isDelivered: req.body.isDelivered,
-      deliveredAt: req.body.deliveredAt,
-      email: req.body.email,
-      user: req.body.user || null,
-    });
+orderRouter.get("/:id", isAuth, orderController.getAllUserOrders);
 
-    const createdProduct = await order.save();
-    res.send(createdProduct);
-  })
-);
-
-orderRouter.post(
-  "/products-total-price",
-  expressAsyncHandler(async (req, res) => {
-    //@ts-ignore
-    const productsIds = req.body.productsList.map((el) => el.productId);
-    const productsList = await Product.find({
-      _id: { $in: productsIds },
-    });
-
-    if (!productsList) {
-      res.status(404).send("Product doesn't exist");
-      return;
-    }
-
-    const singleProductArray = productsList.map((el) => ({
-      id: el._id,
-      price: el.price,
-    }));
-
-    const mergedArray = mergeTwoArraysOfObject(singleProductArray, req.body.productsList);
-
-    const totalPrice = mergedArray.map((el) => el.qty * el.price).reduce((a, b) => a + b, 0);
-
-    res.send({ price: totalPrice + SHIPPING_PRICE });
-  })
-);
-
-orderRouter.get(
-  "/:id",
-  isAuth,
-  expressAsyncHandler(async (req, res) => {
-    //@ts-ignore
-    const skip = parseInt(req.query.skip);
-    //@ts-ignore
-    const limit = parseInt(req.query.limit);
-
-    const orders = await Order.find({ user: req.params.id }).sort({ _id: -1 }).skip(skip).limit(limit);
-
-    if (!orders) {
-      res.status(404).send({ message: "Orders not Found" });
-      return;
-    }
-    res.send(orders);
-  })
-);
+orderRouter.post("/", orderController.postOrder);
+orderRouter.post("/products-total-price", orderController.totalProductsPrice);
 
 export default orderRouter;
