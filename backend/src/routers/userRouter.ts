344792d31ea507b1b09438d9e5ef52eeb0a4acb0
Ryszard Matula <rmatula@us.edu.pl>
2021-03-30 19:56:19 +0200

    Backend | User Controller

--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -1,122 +1,15 @@
 import express from "express";
-import expressAsyncHandler from "express-async-handler";
-import argon2 from "argon2";
-import User from "../models/userModel";
-import Order from "../models/orderModel";
-import { generateToken, isAuth, isAdmin } from "../utils";
+import * as userController from "../controllers/userController";
+import { isAdmin, isAuth } from "../utils";
 
 const userRouter = express.Router();
 
-userRouter.post(
-  "/signin",
-  expressAsyncHandler(async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    const x = await argon2.hash(req.body.password);
-    console.log(x);
-    if (user) {
-      const valid = await argon2.verify(user.password, req.body.password);
-      if (valid) {
-        res.send({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          isAdmin: user.isAdmin,
-          token: generateToken(user),
-        });
-        return;
-      }
-    }
-    res.status(401).send({ message: "Invalid email or password" });
-  })
-);
+userRouter.get("/:id", isAuth, isAdmin, userController.getUser);
+userRouter.get("/:id/profile", isAuth, userController.getProfileInfo);
 
-userRouter.post(
-  "/register",
-  expressAsyncHandler(async (req, res) => {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: await argon2.hash(req.body.password),
-    });
-    const createdUser = await user.save();
-    res.send({
-      _id: createdUser._id,
-      name: createdUser.name,
-      email: createdUser.email,
-      isAdmin: createdUser.isAdmin,
-      token: generateToken(createdUser),
-    });
-  })
-);
+userRouter.post("/signin", userController.signin);
+userRouter.post("/register", userController.register);
 
-userRouter.get(
-  "/:id",
-  isAuth,
-  isAdmin,
-  expressAsyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-      res.send({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        createrdAt: user.createdAt,
-      });
-    } else {
-      res.status(404).send({ message: "User not Found" });
-    }
-  })
-);
+userRouter.patch("/profile", isAuth, userController.patchProfileInfo);
 
-userRouter.get(
-  "/:id/profile",
-  isAuth,
-  expressAsyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    const orders = await Order.find({ user: req.params.id });
-
-    if (!user) {
-      res.status(404).send({ message: "User not Found" });
-      return;
-    }
-
-    if (!orders) {
-      res.status(404).send({ message: "Orders not Found" });
-      return;
-    }
-
-    res.send({
-      user: {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        createrdAt: user.createdAt,
-      },
-      orders,
-    });
-  })
-);
-
-userRouter.put(
-  "/profile",
-  isAuth,
-  expressAsyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
-    if (user) {
-      user.name = req.body.name || user.name;
-      user.email = req.body.email || user.email;
-      if (req.body.password) {
-        user.password = await argon2.hash(req.body.password);
-      }
-      const updatedUser = await user.save();
-      res.send({
-        _id: updatedUser._id,
-        name: updatedUser.name,
-        email: updatedUser.email,
-        isAdmin: updatedUser.isAdmin,
-        token: generateToken(updatedUser),
-      });
-    }
-  })
-);
 export default userRouter;
